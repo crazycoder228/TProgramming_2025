e9083762f6d4c39f89630d8f7f72ff531468575d
crazycoder228 <xxxmuxahxxx@gmail.com>
2025-05-15 23:47:03 +0300

    Задание 3, вариант 18

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Println("Марков Михаил Викторович")
+	// Вывод результата выполнения функции calculate с точностью до 4 знаков после запятой
+	fmt.Printf("y = %.4f\n", calculate(2.5, 4.6, 1.2))
+}
+
+func calculate(a, b, x float64) float64 {
+	// Вычисление (a + b*x)
+	abx := a + b*x
+
+	// Вычисление числителя: (a + b*x)^2.5
+	numerator := math.Pow(abx, 2.5)
+
+	// Вычисление знаменателя: 1 + log10(a + b*x)
+	denominator := 1 + math.Log10(abx)
+
+	// Итоговый результат
+	y := numerator / denominator
+
+	// Возврат резултата
+	return y
 }
